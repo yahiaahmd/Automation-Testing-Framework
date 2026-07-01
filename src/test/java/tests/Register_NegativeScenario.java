de9cb87a6b73916694264f59b6211d9752c375c3
Yahia Ahmed <yelkholy2002@gmail.com>
2026-07-01 08:31:38 +0300

    Add data driven to register negative scenario

--- src/test/java/tests/Register_NegativeScenario.java
+++ src/test/java/tests/Register_NegativeScenario.java
@@ -1,21 +1,30 @@
 package tests;
 
+import java.io.IOException;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import dataModels.registerData;
 import pages.HomePage;
 import pages.RegisterPage;
+import utils.JsonUtils;
 
 public class Register_NegativeScenario extends TestBase {
 	RegisterPage registerObject;
 	HomePage homeObject;
-  @Test
-  public void registerWithExistEmail() {
+	@DataProvider(name = "negativeProvider")
+	public Object [][] getNegativeeData() throws IOException {
+		String path = "src/test/java/testData/register_data.json";
+		return JsonUtils.getSubTestData(path,"negative", registerData.class);}
+  @Test (dataProvider = "negativeProvider")
+  public void registerWithExistEmail(registerData data) {
 	  registerObject = new RegisterPage(driver);
 	  homeObject = new HomePage(driver);
 	  homeObject.openRegisterPage();
 	  Assert.assertEquals(registerObject.signUpMsg.getText(), "New User Signup!");
-	  registerObject.userCanRegister("Yahia", "testing2301@gmail.com");
+	  registerObject.userCanRegister(data.getName(), data.getEmail());
 	  Assert.assertTrue(registerObject.errorMsg.isDisplayed());
 }
 }
